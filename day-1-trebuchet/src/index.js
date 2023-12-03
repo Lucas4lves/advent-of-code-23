a59dfd56899fcd1afa73661f32e471a4bbf643ce
lucas4lves <zombnfl@gmail.com>
2023-12-03 20:39:02 -0300

    Mais ajustes

--- day-1-trebuchet/src/index.js
+++ day-1-trebuchet/src/index.js
@@ -1,76 +1,11 @@
 import { readFileSync } from 'fs'
-import { numericWords } from'./utils/wordsDict.js';
-import { isNumeric } from './utils/helpers/isNumeric.js';
-
+import { tokenize } from './utils/helpers/tokenize.js';
 
 const input = readFileSync("./input", "utf-8");
 const lines = input.split("\n");
 
-const tokenize = (line, debug=false) => {
-
-	if(line.length <= 0)
-	{
-		return 0;
-	}
-	
-	let tkBuffer = "";
-	let outputList = [];
-
-	for(let o = 0; o < line.length; o++)
-	{
-		if(!isNumeric(line[o]))
-		{
-			tkBuffer += line[o];
-		}else{
-			outputList.push(line[o]);
-			tkBuffer = "";
-		}
-
-		if(typeof numericWords[`${tkBuffer}`] === 'number')
-		{
-			outputList.push(tkBuffer);
-			tkBuffer = "";
-		}else{
-			let keys = Object.keys(numericWords);
-
-			for(let i = 0; i < keys.length; i++)
-			{
-				if(tkBuffer.includes(keys[i]))
-				{
-					if(debug)
-					{
-						console.log(`Such buffer [${tkBuffer}] includes ${keys[i]}`);
-					}
-					outputList.push(tkBuffer.slice(tkBuffer.length - keys[i].length, tkBuffer.length))
-					tkBuffer = "";
-				}
-			}
-
-		}
-
-	}
-	
-	if(debug)
-	{
-		return {
-			line,
-			outputList,
-		};
-	}
-
-	return [outputList[0] + outputList[outputList.length -1]];
-}
-
-//let result = tokenize(lines[0]);
 let result = lines.map(line =>{
 	return tokenize(line, false);
 })
 
-/*
-	*
-for(let p = 0; p < 500; p++)
-{
-	result.push(tokenize(lines[p]));
-}
-	* */
 console.log(result);
---
+++ day-1-trebuchet/src/utils/helpers/tokenize.js
@@ -0,0 +1,58 @@
+import { numericWords } from'../wordsDict.js';
+import { isNumeric } from './isNumeric.js';
+
+
+export const tokenize = (line, debug=false) => {
+
+	if(line.length <= 0)
+	{
+		return 0;
+	}
+	
+	let tkBuffer = "";
+	let outputList = [];
+
+	for(let o = 0; o < line.length; o++)
+	{
+		if(!isNumeric(line[o]))
+		{
+			tkBuffer += line[o];
+		}else{
+			outputList.push(line[o]);
+			tkBuffer = "";
+		}
+
+		if(typeof numericWords[`${tkBuffer}`] === 'number')
+		{
+			outputList.push(tkBuffer);
+			tkBuffer = "";
+		}else{
+			let keys = Object.keys(numericWords);
+
+			for(let i = 0; i < keys.length; i++)
+			{
+				if(tkBuffer.includes(keys[i]))
+				{
+					if(debug)
+					{
+						console.log(`Such buffer [${tkBuffer}] includes ${keys[i]}`);
+					}
+					outputList.push(tkBuffer.slice(tkBuffer.length - keys[i].length, tkBuffer.length))
+					tkBuffer = "";
+				}
+			}
+
+		}
+
+	}
+	
+	if(debug)
+	{
+		return {
+			line,
+			outputList,
+		};
+	}
+
+	return [outputList[0] + outputList[outputList.length -1]];
+}
